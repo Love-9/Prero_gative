540bf20ed661b801c71e52d8c784af00dc3a8417
Manan Bhasin <mananbhasin5657@gmail.com>
2024-04-25 11:57:55 +0530

    Update Controller

--- Controller/CouponController.js
+++ Controller/CouponController.js
@@ -1,42 +1,47 @@
-const Coupons=require('../Models/Coupon.js');
-const Admin=require('../Models/admin.js');
-const Courses=require('../Models/course_listing.js');
+const Coupons = require("../Models/Coupon.js");
+const Admin = require("../Models/admin.js");
+const Courses = require("../Models/course_listing.js");
+const flash = require("connect-flash");
 
-module.exports.index=async (req, res) => {
-    let allCoupons = await Coupons.find({});
-    res.render("Coupons.ejs", { allCoupons });
-}
+module.exports.index = async (req, res) => {
+  let allCoupons = await Coupons.find({});
+  res.render("Coupons.ejs", { allCoupons });
+};
 
-module.exports.showCoupon=async (req, res) => {
-    let { id } = req.params;
-    let Coupon = await Coupons.findById(id).populate("course");
-    res.render("Show_Coupon.ejs", { Coupon });
-}
+module.exports.showCoupon = async (req, res) => {
+  let { id } = req.params;
+  let Coupon = await Coupons.findById(id).populate("course");
+  res.render("Show_Coupon.ejs", { Coupon });
+};
 
-module.exports.renderNewForm=async (req, res) => {
-    let allCourses = await Courses.find({});
-    res.render("Create.ejs", { allCourses });
-}
+module.exports.renderNewForm = async (req, res) => {
+  let allCourses = await Courses.find({});
+  res.render("Create.ejs", { allCourses });
+};
 
 module.exports.createCoupon = async (req, res) => {
   try {
-    // Convert the coupon name to lowercase
-    req.body.Coupon.Name = req.body.Coupon.Name.toLowerCase();
-
     // Create a new coupon
     const newCoupon = new Coupons(req.body.Coupon);
-    let savedCoupon = await newCoupon.save();
+    const checkCoupon = await Coupons.findOne({ Name: newCoupon.Name });
 
-    // Retrieve the Admin document
-    let admin = await Admin.findOne();
+    if (!checkCoupon) {
+      let savedCoupon = await newCoupon.save();
 
-    // Update the Admin's coupons array
-    admin.coupons.push(savedCoupon._id);
+      // Retrieve the Admin document
+      let admin = await Admin.findOne();
 
-    // Save the updated Admin document
-    await admin.save();
+      // Update the Admin's coupons array
+      admin.coupons.push(savedCoupon._id);
 
-    res.redirect("/admin");
+      // Save the updated Admin document
+      await admin.save();
+
+      res.redirect("/admin");
+    } else {
+      req.flash("error", "Your Coupon already exist");
+      return res.redirect(`/admin/createCoupon`);
+    }
   } catch (err) {
     // Handle error
     console.error(err);
@@ -44,44 +49,43 @@ module.exports.createCoupon = async (req, res) => {
   }
 };
 
+module.exports.renderEditForm = async (req, res) => {
+  let { id } = req.params;
+  let allCourses = await Courses.find({});
+  const Coupon = await Coupons.findById(id);
+  res.render("EditCoupon.ejs", { Coupon, allCourses });
+};
 
-module.exports.renderEditForm=async (req, res) => {
-    let { id } = req.params;
-    let allCourses = await Courses.find({});
-    const Coupon = await Coupons.findById(id);
-    res.render("EditCoupon.ejs", { Coupon, allCourses });
-}
+module.exports.updateCoupon = async (req, res) => {
+  const { id } = req.params;
+  const upDatedFields = req.body.Coupon;
+  const UpdatedCoupon = await Coupons.findByIdAndUpdate(id, upDatedFields, {
+    new: true,
+  });
+  res.redirect("/admin");
+};
 
-module.exports.updateCoupon=async (req, res) => {
+module.exports.deleteCoupon = async (req, res) => {
+  try {
     const { id } = req.params;
-    const upDatedFields = req.body.Coupon;
-    const UpdatedCoupon = await Coupons.findByIdAndUpdate(id, upDatedFields, {
-      new: true,
-    });
-    res.redirect("/admin");
-}
 
-module.exports.deleteCoupon=async (req, res) => {
-    try {
-      const { id } = req.params;
-  
-      const deletedCoupon = await Coupons.findByIdAndDelete(id);
-  
-      // Find the Admin document
-      let admin = await Admin.findOne();
-  
-      // Remove the deleted coupon's ObjectId from the Admin's coupons array
-      admin.coupons = admin.coupons.filter(
-        (couponId) => couponId.toString() !== id
-      );
-  
-      // Save the updated Admin document
-      await admin.save();
-  
-      res.redirect("/admin/Coupons");
-    } catch (err) {
-      // Handle error
-      console.error(err);
-      res.status(500).send("Internal Server Error");
-    }
+    const deletedCoupon = await Coupons.findByIdAndDelete(id);
+
+    // Find the Admin document
+    let admin = await Admin.findOne();
+
+    // Remove the deleted coupon's ObjectId from the Admin's coupons array
+    admin.coupons = admin.coupons.filter(
+      (couponId) => couponId.toString() !== id
+    );
+
+    // Save the updated Admin document
+    await admin.save();
+
+    res.redirect("/admin/Coupons");
+  } catch (err) {
+    // Handle error
+    console.error(err);
+    res.status(500).send("Internal Server Error");
   }
+};
